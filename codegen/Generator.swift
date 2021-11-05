b77a4118445954eced67391997e8941383281b2f
Thomas Terry <darkforestsilence@gmail.com>
2021-11-05 11:43:48 -0500

    Add comments to codegen

--- codegen/Generator.swift
+++ codegen/Generator.swift
@@ -14,12 +14,10 @@ struct Generator {
     let ast: Program
     let module = Module(name: "main")
     let builder: IRBuilder
-    
-    
-    
+    let machine: TargetMachine
+
     var functions: [String: LLVMFunction] = [:]
     
-    let machine: TargetMachine
         
     init(ast: Program) throws {
         self.ast =  ast
@@ -29,6 +27,7 @@ struct Generator {
     }
         
     mutating func generate(to file: URL) throws {
+        // pre-look through functions for their definitions and add them to the current table
         for tu in ast.translationUnits {
             if case let .Function(f) = tu {
                 let fn = builder.addFunction(f.name, type: FunctionType([], IntType.int32))
@@ -36,33 +35,48 @@ struct Generator {
             }
         }
         
+        // generate the function code
         for tu in ast.translationUnits {
             if case let .Function(f) = tu {
                 try generateFunction(f)
             }
         }
         
-        let pipeline = PassPipeliner(module: module)
         
+        // output unoptimized llvm assembly
         try module.print(to: file.deletingPathExtension().appendingPathExtension("ll").path)
         
+        // set up module optimizer
+        let pipeline = PassPipeliner(module: module)
+
         pipeline.addStandardModulePipeline("mod")
         pipeline.addStandardFunctionPipeline("fun")
         
+        // run optimizations
         pipeline.execute()
         
+        // output the optimized llvm assembly
         try module.print(to: file.deletingPathExtension().appendingPathExtension("llopt").path)
         
+        // output object file
         try machine.emitToFile(module: module, type: .object, path: file.path)
+        
+        // output platform assembly
+        try machine.emitToFile(module: module, type: .assembly,
+                               path: file.deletingPathExtension()
+                                .appendingPathExtension("s").path)
     }
     
     func generateFunction(_ function: Function) throws {
+        // get the module function reference
         let fn = try functions[function.name] ?? { throw AssemblerError.invalidAssembly }()
         
+        // add a unnamed block to the function
         let entry = BasicBlock()
         fn.append(entry)
         builder.positionAtEnd(of: entry)
         
+        // generate body
         for stmt in function.body {
             try generateStatement(stmt)
         }
@@ -72,6 +86,7 @@ struct Generator {
         switch statement {
         case .expression(_):
             return
+        // generate a return from the function
         case .return(let expression):
             builder.buildRet(try generateExpression(expression))
         case .empty:
@@ -81,12 +96,15 @@ struct Generator {
     
     func generateExpression(_ expression: Expression) throws -> IRValue {
         switch expression {
+        // create a llvm constant
         case .number(let int):
             return IntType.int32.constant(int)
         case .operation(let left, let op, let right):
+            // pre-generate each sub-expression
             let l = try generateExpression(left)
             let r = try generateExpression(right)
             
+            // build this level of the expression based on the operator
             switch op {
             case .plus:
                 return builder.buildAdd(l, r)
@@ -99,6 +117,7 @@ struct Generator {
             case .mod:
                 return builder.buildRem(l, r)
             }
+        // lookup the function and create a call to it
         case .call(let name):
             let fn = try functions[name] ?? { throw AssemblerError.invalidAssembly }()
             return builder.buildCall(fn, args: [])
