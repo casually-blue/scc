7ef06bf2067bfad20890ab289738d3f1f2e29d3f
Thomas Terry <darkforestsilence@gmail.com>
2021-11-06 10:51:44 -0500

    Moved files and created Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.5.0
+import PackageDescription
+
+let package = Package(
+        name: "scc",
+        products: [
+                .executable(name: "scc", targets: ["Compiler"]),
+        ],
+        dependencies: [
+                .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+                .package(path: "/home/admin_/LLVMSwift"),
+        ],
+        targets: [
+                .executableTarget(
+                        name: "Compiler",
+                        dependencies: [
+                                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                                .product(name: "LLVM", package: "LLVMSwift"),
+                        ]
+                        )
+])
